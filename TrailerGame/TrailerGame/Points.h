71e4db976dbeb3adb134904d11171c8f7106ac2c
Woo Hyun Jin <woohyunjin@gmail.com>
2012-11-23 16:20:03 -0800

    Committing the points class again

--- TrailerGame/TrailerGame/Points.h
+++ TrailerGame/TrailerGame/Points.h
@@ -17,6 +17,7 @@
 @property (nonatomic) int movieId;
 
 + (NSMutableArray *) GetGamePoints;
++ (void) AddGamePoints;
 
 @end
 
---
+++ TrailerGame/TrailerGame/Points.m
@@ -0,0 +1,29 @@
+//
+//  Points.m
+//  TrailerGame
+//
+//  Created by Woo Hyun Jin on 11/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Points.h"
+
+@implementation Points
+
+@synthesize movieId;
+@synthesize pointsEarned;
+@synthesize timeSpent;
+@synthesize movieTitle;
+@synthesize posterName;
+
++ (NSMutableArray *) GetGamePoints
+{
+    
+}
+
++ (void) AddGamePoints
+{
+    
+}
+
+@end
