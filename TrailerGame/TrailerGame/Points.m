96a4c79127018a66f0b921d807dd2a4ddd6a29dd
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2012-11-23 16:40:44 -0800

    added game point sample

--- TrailerGame/TrailerGame/Points.m
+++ TrailerGame/TrailerGame/Points.m
@@ -16,9 +16,43 @@
 @synthesize movieTitle;
 @synthesize posterName;
 
++ (NSMutableArray *) GetGamePointsSample
+{
+    NSMutableArray * list = [[NSMutableArray alloc]init];
+    Points * point = [[Points alloc]init];
+    point.movieId = 1;
+    point.pointsEarned = 10;
+    point.timeSpent = 65;
+    point.movieTitle = @"Skyfall";
+    point.posterName = @"220px-Skyfall_poster.jpg";
+    [list addObject:point];
+    
+    point.movieId = 2;
+    point.pointsEarned = 20;
+    point.timeSpent = 128;
+    point.movieTitle = @"Iron Man 3";
+    point.posterName = @"220px-Iron_Man_3_teaser_poster.jpg";
+    [list addObject:point];
+    
+    point.movieId = 1;
+    point.pointsEarned = 10;
+    point.timeSpent = 609;
+    point.movieTitle = @"The Dark Knight Rises";
+    point.posterName = @"220px-Dark_Knight.jpg";
+    [list addObject:point];
+    
+    point.movieId = 1;
+    point.pointsEarned = 10;
+    point.timeSpent = 45;
+    point.movieTitle = @"The Lion King";
+    point.posterName = @"220px-The_Lion_King_poster.jpg";
+    [list addObject:point];
+    
+    return list;
+}
+
 + (NSMutableArray *) GetGamePoints
 {
-    
 }
 
 + (void) AddGamePoints
