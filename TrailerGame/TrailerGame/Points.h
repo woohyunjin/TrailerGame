7754602ae3d7905512a873b209019c2d10fccc03
Woo Hyun Jin <woohyunjin@gmail.com>
2012-11-23 16:09:51 -0800

    Header file for the Points class

---
+++ TrailerGame/TrailerGame/Points.h
@@ -0,0 +1,23 @@
+//
+//  Points.h
+//  TrailerGame
+//
+//  Created by Woo Hyun Jin on 11/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Points : NSObject
+
+@property (nonatomic, strong) NSString *movieTitle;
+@property (nonatomic, strong) NSString *posterName;
+@property (nonatomic) NSTimeInterval timeSpent;
+@property (nonatomic) int pointsEarned;
+@property (nonatomic) int movieId;
+
++ (NSMutableArray *) GetGamePoints;
+
+@end
+
+
